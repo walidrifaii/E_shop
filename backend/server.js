5369489b060fcbaee7affafaf3ccc7f0091e4236
walidrifaii <walidrifaii53@gmail.com>
2024-11-23 17:51:45 +0200

    fix error

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ process.on('uncaughtException', (err)=>{
 
 
 // config 
-if(process.env. !== 'PRODUCTION'){
+if(process.env.NODE_ENV !== 'PRODUCTION'){
     require('dotenv').config({
         path: "config/.env"
     })
