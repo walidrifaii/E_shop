befe7a70ecb94482d5514d10119e09f618dea9a5
walidrifaii <walidrifaii53@gmail.com>
2024-11-29 15:51:17 +0200

    fix server in fronten2d

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
 export const server = "https://e-shop-3f6f.vercel.app/api/v2"
 
-export const backend_url = "https://e-shop-blond-sigma.vercel.app/"
+export const backend_url = "https://e-shop-blond-sigma.vercel.app"
